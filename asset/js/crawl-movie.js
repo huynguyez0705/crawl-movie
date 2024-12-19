5f8c80adb87f974b9a5b7b2a6ce43449baaff465
huynguyez0705 <huy751998@gmail.com>
2024-12-19 09:36:58 +0700

    update

--- asset/js/crawl-movie.js
+++ asset/js/crawl-movie.js
@@ -5,14 +5,11 @@ let allLatestItems = {}
 
 // Hàm lấy dữ liệu từ URL JSON với phân trang
 function fetchJsonData(apiUrl, page) {
-	// Kiểm tra xem URL đã có phần phân trang chưa
 	let url = apiUrl.trim()
 
-	// Nếu URL không có ?page= thì thêm vào
 	if (!url.includes('?page=')) {
 		url += `?page=${page}`
 	} else {
-		// Nếu đã có ?page=, thay thế page={i} bằng giá trị mới
 		url = url.replace(/([?&])page=\d+/, `$1page=${page}`)
 	}
 
@@ -23,16 +20,17 @@ function fetchJsonData(apiUrl, page) {
 		return response.json()
 	})
 }
+
 function showToast(message, type = 'success') {
 	const toast = document.createElement('div')
 	toast.classList.add('toast')
 
 	if (type === 'success') {
-		toast.style.backgroundColor = '#4caf50' // Màu xanh cho thành công
+		toast.style.backgroundColor = '#4caf50'
 	} else if (type === 'error') {
-		toast.style.backgroundColor = '#f44336' // Màu đỏ cho lỗi
+		toast.style.backgroundColor = '#f44336'
 	} else if (type === 'info') {
-		toast.style.backgroundColor = '#2196F3' // Màu xanh dương cho thông tin
+		toast.style.backgroundColor = '#2196F3'
 	}
 
 	toast.textContent = message
@@ -45,10 +43,10 @@ function showToast(message, type = 'success') {
 		setTimeout(() => toast.remove(), 500)
 	})
 	toast.appendChild(closeBtn)
-	// Thêm toast vào container
+
 	const toastContainer = document.querySelector('.toast-container')
 	toastContainer.appendChild(toast)
-	// Tự động ẩn toast sau 5 giây
+
 	setTimeout(() => {
 		toast.classList.add('hide')
 		setTimeout(() => toast.remove(), 500)
@@ -59,18 +57,17 @@ function showToast(message, type = 'success') {
 function getUrls(jsonData, nameList, source) {
 	let namesToCheck = nameList.split('\n').map(name => name.trim().toLowerCase())
 	let latestItems = {}
-	let hasChieuRap = false // Biến kiểm tra có phim chiếu rạp hay không
+	let hasChieuRap = false
 	const isChieuRapOnly = document.getElementById('crawlTheater').checked
 
-	let items = source === 'kkphim' ? jsonData.data?.items || [] : jsonData.items || []
+	let items = source === 'kkphim' || source === 'ophim' ? jsonData.data?.items || [] : jsonData.items || []
 
 	if (!Array.isArray(items)) {
 		console.error('Dữ liệu items không hợp lệ:', items)
-		showToast('Dữ liệu items không hợp lệ:', error)
+		showToast('Dữ liệu items không hợp lệ:', 'error')
 		return { nonMatchingUrls: [], matchingUrls: [], latestItems: {} }
 	}
 
-	// Hàm xử lý chung cho tất cả các item
 	function processItem(item) {
 		const itemName = item.name.toLowerCase()
 		const itemTime = new Date(item.modified.time)
@@ -82,27 +79,23 @@ function getUrls(jsonData, nameList, source) {
 		}
 	}
 
-	// Lọc và xử lý item dựa trên chế độ đã chọn
 	if (isChieuRapOnly) {
 		items.forEach(item => {
 			if (item.chieurap === true) {
-				processItem(item) // Xử lý item chiếu rạp
+				processItem(item)
 			}
 		})
 
-		// Nếu không có phim chiếu rạp nào, hiển thị thông báo lỗi
 		if (!hasChieuRap) {
 			return { nonMatchingUrls: [], matchingUrls: [], latestItems: {} }
 		}
 	} else {
-		// Nếu không chọn "Chỉ Crawl Phim Chiếu Rạp", xử lý tất cả phim
-		items.forEach(item => processItem(item)) // Xử lý tất cả các item
+		items.forEach(item => processItem(item))
 	}
 
 	let nonMatchingUrls = []
 	let matchingUrls = []
 
-	// Xuất ra các URL
 	Object.values(latestItems).forEach(item => {
 		const slug = item.slug
 		const id = item._id
@@ -111,10 +104,11 @@ function getUrls(jsonData, nameList, source) {
 		const origin_name = item.origin_name
 		const year = item.year
 
-		// Cấu trúc URL theo nguồn
-		const url = source === 'kkphim' ? `https://phimapi.com/phim/${slug}|${id}|${modifiedTime}|${name}|${origin_name}|${year}` : `https://ophim1.com/phim/${slug}|${id}|${modifiedTime}|${name}|${origin_name}|${year}`
+		const url =
+			source === 'kkphim'
+				? `https://phimapi.com/phim/${slug}|${id}|${modifiedTime}|${name}|${origin_name}|${year}`
+				: `https://ophim1.com/phim/${slug}|${id}|${modifiedTime}|${name}|${origin_name}|${year}`
 
-		// Phân loại theo tên
 		if (namesToCheck.includes(item.name.toLowerCase())) {
 			matchingUrls.push({ name: item.name, url: url })
 		} else {
@@ -125,7 +119,6 @@ function getUrls(jsonData, nameList, source) {
 	return { nonMatchingUrls, matchingUrls, latestItems }
 }
 
-// Hàm hiển thị danh sách URL trên trang web
 function displayUrls() {
 	const apiUrl = document.getElementById('apiUrl').value
 	const nameList = document.getElementById('nameList').value
@@ -134,46 +127,41 @@ function displayUrls() {
 	const endPage = parseInt(document.getElementById('endPage').value)
 	const uniqueNamesCount = document.getElementById('uniqueNamesCount')
 	uniqueNamesCount.textContent = 'Đang tải...'
-	// Hàm xử lý lấy dữ liệu từ tất cả các trang
+
 	function fetchAllPages(currentPage) {
-		const totalPages = endPage - startPage + 1 // Tổng số trang cần tải
+		const totalPages = endPage - startPage + 1
 		const progressElement = document.getElementById('loadingProgress')
 		const progressWrapper = document.getElementById('progressWrapper')
 
-		// Hiển thị thanh tiến trình khi bắt đầu tải
 		progressWrapper.style.display = 'block'
 
-		// Cập nhật thông tin trang hiện tại
 		const currentPageElement = document.getElementById('currentPage')
 		currentPageElement.classList.add('jump')
-		currentPageElement.textContent = currentPage
+		currentPageElement.textContent = endPage
 		setTimeout(() => {
 			currentPageElement.classList.remove('jump')
 		}, 300)
-		// Tính toán tiến độ
+
 		const progressValue = ((currentPage - startPage) / totalPages) * 100
 		progressElement.value = progressValue
 
 		if (currentPage > endPage) {
-			// Đã qua hết các trang, hiển thị kết quả
 			uniqueNamesCount.textContent = `Số lượng phim hiện tại: ${Object.keys(allLatestItems).length}`
 			showToast(`Hoàn thành ${currentPage} Page`)
-			return // Kết thúc khi đã xử lý tất cả các trang
+			return
 		}
 
 		fetchJsonData(apiUrl, currentPage)
 			.then(jsonData => {
-				const { nonMatchingUrls, matchingUrls, latestItems } = getUrls(jsonData, nameList, sourceSelect, currentPage) //
+				updateCrawlSummary(jsonData)
+				const { nonMatchingUrls, matchingUrls, latestItems } = getUrls(jsonData, nameList, sourceSelect)
 				allNonMatchingUrls = allNonMatchingUrls.concat(nonMatchingUrls)
 				allMatchingUrls = allMatchingUrls.concat(matchingUrls)
 				Object.assign(allLatestItems, latestItems)
 
-				updateTableWithMessage('nonMatchingTable', nonMatchingUrls)
-				updateTableWithMessage('matchingTable', matchingUrls)
-				if (nonMatchingUrls.length === 0 && matchingUrls.length === 0) {
-					showToast(`${apiUrl}?page=${currentPage} không có phim chiếu rạp`, 'error')
-				}
-				// Gọi tiếp trang tiếp theo
+				updateTableWithMessage('nonMatchingTable', nonMatchingUrls, 'Không có phim không trùng khớp.')
+				updateTableWithMessage('matchingTable', matchingUrls, 'Không có phim trùng khớp.')
+
 				fetchAllPages(currentPage + 1)
 			})
 			.catch(error => {
@@ -182,11 +170,9 @@ function displayUrls() {
 			})
 	}
 
-	// Bắt đầu lấy dữ liệu từ trang bắt đầu
 	fetchAllPages(startPage)
 }
 
-// Hàm cập nhật bảng với dữ liệu hoặc thông báo nếu không có dữ liệu
 function updateTableWithMessage(tableId, items, message) {
 	const tableBody = document.getElementById(tableId)
 
@@ -211,35 +197,50 @@ function updateTableWithMessage(tableId, items, message) {
 	}
 }
 
-// Hàm kiểm tra và hiển thị/ẩn radio button dựa trên lựa chọn nguồn
 function toggleCrawlModeVisibility() {
 	const sourceSelect = document.getElementById('sourceSelect')
 	const crawlTheaterWrapper = document.getElementById('crawlModeWrapper')
 	const buttonLnk = document.getElementById('buttonIn_list')
 	const selectedSource = sourceSelect.value
 
-	// Hiển thị checkbox khi chọn KKPhim, ẩn khi chọn nguồn khác
 	if (selectedSource === 'kkphim') {
-		crawlTheaterWrapper.style.display = 'block'
 		buttonLnk.style.display = 'flex'
 		document.getElementById('apiUrl').value = 'https://phimapi.com/v1/api/danh-sach/phim-le'
 	} else {
-		crawlTheaterWrapper.style.display = 'none'
-		buttonLnk.style.display = 'none'
-		document.getElementById('apiUrl').value = 'https://ophim1.com/danh-sach/phim-moi-cap-nhat'
-		// Ẩn radio khi chọn nguồn khác
+		buttonLnk.style.display = 'flex'
+		document.getElementById('apiUrl').value = 'https://ophim1.com/v1/api/danh-sach/phim-le'
 	}
 }
+
 function getUrlbtn(element) {
-	const urlLink = 'https://phimapi.com/v1/api/danh-sach/'
+	const urlLink =
+		document.getElementById('sourceSelect').value === 'kkphim'
+			? 'https://phimapi.com/v1/api/danh-sach/'
+			: 'https://ophim1.com/v1/api/danh-sach/'
+
 	const categoryUrls = {
 		getUrlHH: `${urlLink}hoat-hinh`,
 		getUrlsingle: `${urlLink}phim-le`,
 		getUrlseries: `${urlLink}phim-bo`
 	}
+
 	const category = element.id
 	categoryUrls[category] ? (document.getElementById('apiUrl').value = categoryUrls[category]) : NaN
 }
+function updateCrawlSummary(jsonData) {
+	const totalItems = jsonData.data.params.pagination.totalItems || 0
+	const itemsPerPage = jsonData.data.params.pagination.totalItemsPerPage || 0
+	const currentPage = jsonData.data.params.pagination.currentPage || 0
+	const totalPages = jsonData.data.params.pagination.totalPages || 0
+
+	document.getElementById('totalItems').textContent = totalItems
+	document.getElementById('itemsPerPage').textContent = itemsPerPage
+	document.getElementById('currentP').textContent = currentPage
+	document.getElementById('totalPages').textContent = totalPages
+}
+document.getElementById('sourceSelect').addEventListener('change', toggleCrawlModeVisibility)
+window.addEventListener('load', toggleCrawlModeVisibility)
+
 document.getElementById('sourceSelect').addEventListener('change', toggleCrawlModeVisibility)
 
 // Kích hoạt sự kiện mặc định khi trang tải (lần đầu tiên)
